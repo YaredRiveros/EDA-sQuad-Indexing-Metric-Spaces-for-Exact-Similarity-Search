31dcb283037c7b28eb01034b2bbf84df78be1b9f
Mariana-Capunay <mariana.capunay@utec.edu.pe>
2025-11-23 09:05:40 -0500

    results

--- main_memory/EPT/test.cpp
+++ main_memory/EPT/test.cpp
@@ -11,8 +11,8 @@ using namespace chrono;
 
 static const vector<double> SELECTIVITIES = {0.02, 0.04, 0.08, 0.16, 0.32};
 static const vector<int>    K_VALUES      = {5, 10, 20, 50, 100};
-static const vector<string> DATASETS      = {"LA", "Words", "Color"};
-
+//static const vector<string> DATASETS      = {"LA", "Words", "Color"};
+static const vector<string> DATASETS      = {"Synthetic"};
 // Parámetros EPT* (l pivots por objeto, cp_scale candidatos PSA)
 // Parámetros EPT* (l pivots por objeto, cp_scale candidatos PSA)
 struct EPT_Params {
